02f682e024ba22e4686b0e7c1d321476c907d5e8
SuperGamer500 <SuperGamer500@github.com>
2026-07-09 14:10:40 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-09T14:10:40.207Z

--- src/main/main.py
+++ src/main/main.py
@@ -93,7 +93,7 @@ def load_and_clean_call_logs(file_path):
 # You must save records consisting of each userId, avgDuration, and numCalls
 # example: 1,105.0,4 - where 1 is the userId, 105.0 is the avgDuration, and 4 is the numCalls.
 def write_user_analytics(csv_file_path):
-    cursor.execute("SELECT userId, s/c as Average, c from (SELECT userId, Sum(endTime-startTime) as s, Count(*) as c from product Group By userId);")
+    cursor.execute("SELECT userId, s/c as Average, c from (SELECT userId, Sum(endTime-startTime) as s, Count(*) as c from callLogs Group By userId);")
     print(cursor.fetchall());
     print("TODO: write_user_analytics")
 
