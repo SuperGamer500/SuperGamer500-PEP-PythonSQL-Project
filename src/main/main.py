6c25d1a4c9e9a63bbbffdf41663f9e6270fa2ca2
SuperGamer500 <SuperGamer500@github.com>
2026-07-09 13:25:13 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-09T13:25:13.114Z

--- src/main/main.py
+++ src/main/main.py
@@ -68,7 +68,7 @@ def main():
 # This function will load the users.csv file into the users table, discarding any records with incomplete data
 def load_and_clean_users(file_path):
     info = ParseCsv(file_path);
-    #cursor.executemany("Insert into users (firstname,lastname) values (?,?)", info)
+    cursor.executemany("Insert into users (firstname,lastname) values (?,?)", info)
     conn.commit()
     print("TODO: load_users")
 
